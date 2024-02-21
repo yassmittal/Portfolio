cf6fb9a9214bd9767fa14d3bf3fa40d11117bf82
Yash Mittal <yashmittalmm@gmail.com>
2024-02-21 23:55:31 +0530

    make projects tabs working

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const sections = document.querySelectorAll('[data-id="section-for-toggle"]')
 const navBarLinks = document.querySelectorAll('.navBarLink');
+const projectTabBtns = document.querySelectorAll('[data-id="tab-btn"]');
+const projects = [...document.querySelectorAll('.projectItem')];
+
+const projectsNum = [
+  [0, 1, 2, 3, 4, 5],
+  [2, 3, 4],
+  [5],
+  [0, 1],
+]
 
 navBarLinks.forEach((navBarLink, index) => {
   navBarLink.addEventListener('click', (e) => {
@@ -12,29 +21,31 @@ navBarLinks.forEach((navBarLink, index) => {
     sections.forEach((section, sectionIndex) => {
       section.classList.remove('visible');
 
-      console.log(sectionIndex)
-      console.log(index)
-      console.log(' ')
-
       if (sectionIndex === index) {
-
         section.classList.add('visible')
       }
     })
 
     navBarLink.classList.add('active');
+  })
+})
 
+projectTabBtns.forEach((projectBtn, projectBtnIndex) => {
+  projectBtn.addEventListener('click', (e) => {
+    e.preventDefault();
 
+    projectTabBtns.forEach((projectBtn) => {
+      projectBtn.classList.remove('active')
+    })
 
+    projectBtn.classList.add('active')
 
+    projects.forEach(project => {
+      project.classList.remove('active')
+    });
 
-    //     navBarLinks.forEach((navBarLink, index) => {
-    //       // !navBarLink.classList.contains('hide') ? navBarLink.classList.add('hide') : "";
-    //     })
-
-    //     sections.forEach((section, sectionIndex) => {
-    //       !section.classList.contains('hide') ? section.classList.add('hide') : "";
-    //     })
-    //     navBarLink.classList.remove('hide');
+    projectsNum[projectBtnIndex].forEach(projectIndex => {
+      projects[projectIndex].classList.add('active')
+    });
   })
 })
