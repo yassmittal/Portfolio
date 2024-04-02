96a41389ece1c1e55dc1d7b09abd94bbc3e8a07f
Yash Mittal <yashmittalmm@gmail.com>
2024-04-02 23:35:16 +0530

    fixed order of projects

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const profileSidebar = document.querySelector('[data-id="profile-sidebar"]');
 const showContantsBtn = document.querySelector('.show-contants-btn')
 
 const projectsNum = [
-  [0, 1, 2, 3, 4, 5],
-  [2, 3, 4],
+  [0, 1, 2, 3, 4, 5, 6],
+  [3, 4, 5],
   [0, 1, 2],
   [5],
 ]
